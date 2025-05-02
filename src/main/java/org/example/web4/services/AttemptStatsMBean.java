44c40597e48780fc652ec322be95f57721fbb2c5
chousik <Zaxap2018@yandex.ru>
2025-05-02 15:51:28 +0300

    repair

--- src/main/java/org/example/web4/services/AttemptStatsMBean.java
+++ src/main/java/org/example/web4/services/AttemptStatsMBean.java
@@ -1,6 +1,7 @@
 package org.example.web4.services;
 
 public interface AttemptStatsMBean {
+    void recordAttempt(boolean hit);
     int getTotalSupplied();
     int getTotalIn();
     int getTotalMisses();
